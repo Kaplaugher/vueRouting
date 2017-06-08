238044273663529b92884c6066ac4daa1e29de35
Kaplaugher <kaplaugher94@gmail.com>
2017-06-08 11:26:33 -0400

    working router

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,17 @@
             </div>
         </div>
         <hr>
+        <app-header></app-header>
         <router-view></router-view>
     </div>
 </template>
 
 <script>
+    import Header from './components/Header.vue';
     export default {
+      components: {
+        appHeader: Header
+      }
     }
 </script>
 
